2d33df077127dc1644c8d790ea9cd87d17d977f3
Alex Miller <fotoflo@gmail.com>
2022-06-16 23:35:51 +0800

    initial commit

---
+++ components/GoogleLoginButton.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../styles/Home.module.css'
+
+function GoogleLoginButton() {
+  return (
+    <h1 className={styles.title}>
+      Welcome to NEXT JS
+    </h1>
+  )
+}
+
+export default GoogleLoginButton
