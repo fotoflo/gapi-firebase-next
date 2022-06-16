6b8da7670191d7c398b70b737ff623e6cf161c70
Alex Miller <fotoflo@gmail.com>
2022-06-17 00:03:00 +0800

    install react-bootstrap, editor format on save

--- components/GoogleLoginButton.js
+++ components/GoogleLoginButton.js
@@ -1,12 +1,17 @@
-import React from 'react'
-import styles from '../styles/Home.module.css'
+import React from "react";
+import styles from "../styles/Home.module.css";
+import { FaGoogle } from "react-icons/fa";
+import { Button } from "react-bootstrap";
 
 function GoogleLoginButton() {
   return (
-    <h1 className={styles.title}>
-      Welcome to NEXT JS
-    </h1>
-  )
+    <>
+      <h1 className={styles.title}>Welcome to NEXT JS</h1>
+      <Button>
+        <FaGoogle /> Login with Google
+      </Button>
+    </>
+  );
 }
 
-export default GoogleLoginButton
+export default GoogleLoginButton;
