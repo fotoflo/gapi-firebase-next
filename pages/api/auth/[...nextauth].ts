6ce441fe8d8ef03593815f9fe2062e9ad7c54fe0
Alex Miller <fotoflo@gmail.com>
2022-06-18 21:54:51 +0800

    had to monkeywrench the firebase package. see comment in nextauth route

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,7 +2,38 @@ import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
 
-import {GITHUB_CLIENT_ID, GITHUB_SECRET, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET} from "../../../config"
+import {
+  GITHUB_CLIENT_ID,
+  GITHUB_SECRET,
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET,
+  firebaseConfig
+} from "../../../config"
+
+import { FirebaseAdapter } from "@next-auth/firebase-adapter"
+
+import { initializeApp, getApp, getApps } from "firebase/app";
+import {
+  getFirestore,
+  collection,
+  query,
+  getDocs,
+  where,
+  limit,
+  doc,
+  getDoc,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  runTransaction,
+  
+} from "firebase/firestore";
+
+!getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore();
+
+// 2-You might then get an error that states "cannot use toDate method of undefined", If this occurred tap into node_modules/@next-auth/firebase-adapter/dist inside index.js file replace the exports.format object with this code which adds a "?" operator after value objec
+// https://stackoverflow.com/questions/69876727/next-auth-google-auth-firebase-adapter
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -17,4 +48,18 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  adapter: FirebaseAdapter({
+    db,
+    collection,
+    query,
+    getDocs,
+    where,
+    limit,
+    doc,
+    getDoc,
+    addDoc,
+    updateDoc,
+    deleteDoc,
+    runTransaction,
+    })
 })
