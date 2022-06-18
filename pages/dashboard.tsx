549a98aded954556cb726317e7ffc6290a7b87d1
Alex Miller <fotoflo@gmail.com>
2022-06-18 15:53:48 +0800

    Move logged in state to a route

---
+++ pages/dashboard.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+import Head from "next/head";
+import { useRouter } from "next/router";
+
+import { Button } from "react-bootstrap";
+
+import { useSession, signOut } from "next-auth/react";
+
+
+import Avatar from "../components/util/Avatar";
+  
+const dashboard = () => {
+  
+  const router = useRouter();
+  const { data: session, status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      // The user is not authenticated, handle it here.
+      router.push("/");
+    },
+  });
+
+  if (status === "loading") {
+    return <p>Loading...</p>
+  }
+
+  console.log({ session });
+
+  return (
+    <>
+      <Avatar src={session.user.image} />
+
+      <p>
+        {session.user.name} Signed in as {session.user.email}
+      </p>
+
+      <Button onClick={() => signOut()}>Sign out</Button>
+    </>
+  );
+}
+
+export default dashboard
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import { useSession, signOut } from "next-auth/react";
 
+import { useRouter } from "next/router";
+
 import Head from "next/head";
 import { Button } from "react-bootstrap";
 import GithubLoginButton from "../components/GithubLoginButton";
@@ -8,21 +10,11 @@ import GoogleLoginButton from "../components/GoogleLoginButton";
 import Avatar from "../components/util/Avatar";
 
 export default function Home() {
+  const router = useRouter();
   const { data: session } = useSession();
 
   if (session) {
-    console.log({ session });
-    return (
-      <>
-        <Avatar src={session.user.image} />
-
-        <p>
-          {session.user.name} Signed in as {session.user.email}
-        </p>
-
-        <Button onClick={() => signOut()}>Sign out</Button>
-      </>
-    );
+    router.push("/dashboard");
   }
 
   return (
